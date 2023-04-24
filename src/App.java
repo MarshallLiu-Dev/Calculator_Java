affaab3e58a3d762240b79a1763e6a5800a565ba
MarshallLiu-Dev <paulomartins12358@gmail.com>
2023-04-24 20:59:22 -0300

    heranca e reuso

--- src/App.java
+++ src/App.java
@@ -12,3 +12,29 @@ public class App {
 
     }
 }
+
+// recurso de poo para reuso de membros em class deferentes 
+
+
+//                          subClasse2    =>   superClass <= subClasse1
+
+//                                                 ||
+
+//                                             subClasse3  
+
+
+//   atributos e metodos da superClass são herdados pelas superClasses 
+
+//   -Em Java, a hernça é feita com a diretiva externos 
+
+//                 Veiculo 
+//                    |
+//           carro         moto 
+//             |
+//             |
+//             |
+//         caminhonete                                             
+//     |                   |     
+//     |                   |
+//     |                   |
+//    SUV                 hatch       
